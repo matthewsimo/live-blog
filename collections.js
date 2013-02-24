dc609e627c54be2f753b3712be6830e7b3fd9727
Matthew Simo <matthew.a.simo@gmail.com>
2013-02-23 23:44:06 -0600

    Add collections & data model for each.

---
+++ collections.js
@@ -0,0 +1,25 @@
+/*
+
+This defines the collections used for the app.
+
+There will be a Posts collection, which will hold all of the posts...
+  A Post will have by default:
+    title: String,
+    post_slug: String,
+    creation_time: Number,
+    revision_time: Number,
+    authors: [String, ...]
+
+
+There will also be an Updates collection, which will hold all of the updates across all posts...
+  An update will have by default:
+    content: String,
+    post_id: String,
+    author: String,
+    creation_time: Number,
+
+*/
+
+
+Posts = new Meteor.collection("posts");
+Updates = new Meteor.collection("updates");
