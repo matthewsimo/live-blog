ece795991d9fcba9c20c4bc872a3e18c886169db
Matthew Simo <matthew.a.simo@gmail.com>
2013-02-24 12:48:08 -0600

    Add meteorite packages, router & moment. Set up the basic routes & their templates.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
+// Publish complete set of sorted posts to all clients
+Meteor.publish('posts', function() {
+  return Posts.find({}, {sort: {name: 1}});
+});
+
+// Publish all updates for requested post_id
+Meteor.publish('updates', function(post_id) {
+  return Updates.find({post_id: post_id}, {sort: {creation_time: 1}});
+});
+
 Meteor.startup(function () {
   // code to run on server at startup
 });
